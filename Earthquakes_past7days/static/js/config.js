c74b32b2a78cf11f57cc11538d980d00b5244a4d
Ou <ous1@nala.roche.com>
2020-10-25 15:03:06 -0700

    updated files

---
+++ Earthquakes_past7days/static/js/config.js
@@ -0,0 +1,2 @@
+// API KEY
+const API_KEY = "pk.eyJ1Ijoic2hvZW1heWJlcmsiLCJhIjoiY2tnNWt6ZTJhMHZjNjJxbDA5czJzYm1oYiJ9.5qQA7jbGE8cQ__fG3RkOUA";
--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,5 +1,27 @@
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
+  // We create the tile layer that will be the background of our map.
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
+    
+
+    // Then we add our 'graymap' tile layer to the map.
+    streets.addTo(map);
+
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://https://raw.githubusercontent.com/shumeiberk/Mapping_earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -41,15 +63,3 @@ cityData.forEach(function(city) {
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
   .addTo(map);
 });
-
-
-
-  // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        accessToken: API_KEY
-    });
-    
-    // Then we add our 'graymap' tile layer to the map.
-    streets.addTo(map);
